9fb781ccd3fd4383fbe4347870520e5310a1b357
bcjaeger <byron.jaeger@gmail.com>
2022-03-17 08:05:06 -0400

    report ready; first draft

--- R/sim_run.R
+++ R/sim_run.R
@@ -4,16 +4,19 @@
 #'
 #' @title
 #'
-#' @param run_seed
-#' @param beta_intercept baseline chance of passing qualification exam
+#' @param n_apply
 #' @param corr_gre_pass correlation b/t GRE score and passing exam
+#' @param corr_gpa_pass
+#' @param corr_gre_gpa
+#' @param run_seed
+#' @param base_prob baseline chance of passing qualification exam
 #'
 sim_run <- function(n_apply,
                     corr_gre_pass,
                     corr_gpa_pass,
                     corr_gre_gpa,
                     run_seed,
-                    base_prob = 0.80) {
+                    base_prob = 2.197225) {
 
  # initialize correlation matrix
  mat_corr <- matrix(data = 0, nrow = 3, ncol = 3)
@@ -29,12 +32,14 @@ sim_run <- function(n_apply,
 
  mat_corr[lower.tri(mat_corr)] <- mat_corr[upper.tri(mat_corr)]
 
- mat_cov <- MBESS::cor2cov(cor.mat = mat_corr, sd = c(50, 0.5, 2))
+ mat_cov <- MBESS::cor2cov(cor.mat = mat_corr, sd = c(50, 0.5, 0.3))
 
  set.seed(run_seed)
 
+ beta_intercept <- logit(base_prob)
+
  mat_application <- rmvnorm(n = n_apply,
-                            mean = c(300, 3, logit(base_prob)),
+                            mean = c(300, 3, beta_intercept),
                             sigma = mat_cov)
 
  colnames(mat_application) <- colnames(mat_corr)
@@ -44,18 +49,18 @@ sim_run <- function(n_apply,
 
  gre_gt_75pct <- data_application |>
   filter(gre > as.numeric(quantile(gre, 0.75))) |>
-  summarize(across(everything(), median),
+  summarize(across(everything(), mean),
             n_admit = n(),
             n_pass = sum(rbinom(n=n(), size=1, prob=pass_prob)))
 
  gpa_gt_3 <- data_application |>
   filter(gpa > 3.0) |>
-  summarize(across(everything(), median),
+  summarize(across(everything(), mean),
             n_admit = n(),
             n_pass = sum(rbinom(n=n(), size=1, prob=pass_prob)))
 
  all <- data_application |>
-  summarize(across(everything(), median),
+  summarize(across(everything(), mean),
             n_admit = n(),
             n_pass = sum(rbinom(n=n(), size=1, prob=pass_prob)))
 
@@ -65,7 +70,6 @@ sim_run <- function(n_apply,
            .id = 'strategy') |>
   mutate(n_apply = n_apply,
          corr_gre_pass = corr_gre_pass,
-         corr_gpa_pass = corr_gpa_pass,
          corr_gre_gpa = corr_gre_gpa,
          run_seed = run_seed,
          base_prob = base_prob,
